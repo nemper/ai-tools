35de20e6ac43cc1f3c97d8e07c164b032efe323c
nemper <nemanja.perun98@gmail.com>
2023-09-21 14:07:33 +0200

    Update Koder.py

--- basic-vectorstore-coder/Koder.py
+++ basic-vectorstore-coder/Koder.py
@@ -105,7 +105,7 @@ def main():
 deployment_environment = os.environ.get("DEPLOYMENT_ENVIRONMENT")
 
 if deployment_environment == "Streamlit":
-    name, authentication_status, username = positive_login(main)
+    name, authentication_status, username = positive_login(main, " ")
 else:
     if __name__ == "__main__":
         main()
