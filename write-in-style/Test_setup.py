52a6fd656631e1e11662a00aa05533c105fc9bd3
nemper <nemanja.perun98@gmail.com>
2023-10-17 12:43:34 +0700

    txt

--- write-in-style/Test_setup.py
+++ write-in-style/Test_setup.py
@@ -12,6 +12,7 @@ from pinecone_text.sparse import BM25Encoder
 from langchain.agents.agent_types import AgentType
 from langchain.agents import create_csv_agent
 from myfunc.mojafunkcija import st_style
+from mojafunkcija import positive_login
 
 
 st.set_page_config(page_title="Multi Tool Chatbot", page_icon="👉", layout="wide")
@@ -97,77 +98,78 @@ if "input_prompt" not in st.session_state:
     st.session_state.input_prompt = True
 
 
-st.subheader("Multi Tool Chatbot - Setup")
-with st.expander("Pročitajte uputstvo 🧜‍♂️"):
-    st.caption(
-        """
-                   Na ovom mestu podesavate parametre sistema za testiranje. Za rad CSV agenta potrebno je da uploadujete csv fajl sa struktuiranim podacima.
-                   Za rad ostalih agenata potrebno je da odlucit eda li cete korisiti originalni prompt ili upit koji formira agent. Takodje, odaberite namespace za svaki metod.
-                   Izborom izlaza odlucujete da li ce se odgovor vratiti direktno iz alata ili ce se korisiti dodatni LLM za formiranje odgovora.
-                   Za hybrid search odredite koeficijent alpha koji odredjuje koliko ce biti zastupljena pretraga po kljucnim recima, a koliko po semantickom znacenju.
-                   Mozete odabrati i broj dokumenata koji se vracaju iz indeksa.
-                    """
-    )
-col1, col2, col3 = st.columns(3)
-with col1:
-    set_namespace()
-    st.caption("Izbor prompta")
-    st.session_state.input_prompt = st.radio(
-        "Da li zelite da koristite vas originalni prompt?",
-        [True, False],
-        help="Ako je odgovor False, onda se koristi upit koji formira Agent",
-    )
-with col2:
-    st.caption("Da li zelite direktan odgovor?")
-    st.session_state.direct_semantic = st.radio(
-        "Semantic search",
-        [True, False],
-        help="Pitanja o Positive uopstena",
-    )
+def main():
+    st.subheader("Multi Tool Chatbot - Setup")
+    with st.expander("Pročitajte uputstvo 🧜‍♂️"):
+        st.caption(
+            """
+                    Na ovom mestu podesavate parametre sistema za testiranje. Za rad CSV agenta potrebno je da uploadujete csv fajl sa struktuiranim podacima.
+                    Za rad ostalih agenata potrebno je da odlucit eda li cete korisiti originalni prompt ili upit koji formira agent. Takodje, odaberite namespace za svaki metod.
+                    Izborom izlaza odlucujete da li ce se odgovor vratiti direktno iz alata ili ce se korisiti dodatni LLM za formiranje odgovora.
+                    Za hybrid search odredite koeficijent alpha koji odredjuje koliko ce biti zastupljena pretraga po kljucnim recima, a koliko po semantickom znacenju.
+                    Mozete odabrati i broj dokumenata koji se vracaju iz indeksa.
+                        """
+        )
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        set_namespace()
+        st.caption("Izbor prompta")
+        st.session_state.input_prompt = st.radio(
+            "Da li zelite da koristite vas originalni prompt?",
+            [True, False],
+            help="Ako je odgovor False, onda se koristi upit koji formira Agent",
+        )
+    with col2:
+        st.caption("Da li zelite direktan odgovor?")
+        st.session_state.direct_semantic = st.radio(
+            "Semantic search",
+            [True, False],
+            help="Pitanja o Positive uopstena",
+        )
 
-    st.session_state.direct_hybrid = st.radio(
-        "Hybrid search",
-        [True, False],
-        help="Pitanja o opisu radnih mesta",
-    )
+        st.session_state.direct_hybrid = st.radio(
+            "Hybrid search",
+            [True, False],
+            help="Pitanja o opisu radnih mesta",
+        )
 
-    st.session_state.direct_self = st.radio(
-        "Self search",
-        [True, False],
-        help="Pitanja o meta poljima",
-    )
+        st.session_state.direct_self = st.radio(
+            "Self search",
+            [True, False],
+            help="Pitanja o meta poljima",
+        )
 
-    st.session_state.direct_csv = st.radio(
-        "CSV search",
-        [True, False],
-        help="Pitanja o struktuiranim podacima",
-    )
+        st.session_state.direct_csv = st.radio(
+            "CSV search",
+            [True, False],
+            help="Pitanja o struktuiranim podacima",
+        )
 
-with col3:
-    st.session_state.uploaded_file = st.file_uploader(
-        "Choose a CSV file", accept_multiple_files=False, type="csv", key="csv_key"
-    )
-    if st.session_state.uploaded_file is not None:
-        with io.open(st.session_state.uploaded_file.name, "wb") as file:
-            file.write(st.session_state.uploaded_file.getbuffer())
+    with col3:
+        st.session_state.uploaded_file = st.file_uploader(
+            "Choose a CSV file", accept_multiple_files=False, type="csv", key="csv_key"
+        )
+        if st.session_state.uploaded_file is not None:
+            with io.open(st.session_state.uploaded_file.name, "wb") as file:
+                file.write(st.session_state.uploaded_file.getbuffer())
 
-    st.session_state.broj_k = st.number_input(
-        "Podesi broj dokumenata koje vraca index - radi za sva tri indexa",
-        min_value=1,
-        max_value=5,
-        value=3,
-        step=1,
-        key="broj_k_key",
-        help="Broj dokumenata koji se vraćaju iz indeksa",
-    )
-    st.session_state.alpha = st.slider(
-        "Podesi odnos izmedju keyword i semantic searcha za hybrid search",
-        0.0,
-        1.0,
-        0.5,
-        0.1,
-        help="Koeficijent koji određuje koliko će biti zastupljena pretraga po ključnim rečima, a koliko po semantičkom značenju. 0-0.4 pretezno Kljucne reci , 0.5 podjednako, 0.6-1 pretezno semanticko znacenje",
-    )
+        st.session_state.broj_k = st.number_input(
+            "Podesi broj dokumenata koje vraca index - radi za sva tri indexa",
+            min_value=1,
+            max_value=5,
+            value=3,
+            step=1,
+            key="broj_k_key",
+            help="Broj dokumenata koji se vraćaju iz indeksa",
+        )
+        st.session_state.alpha = st.slider(
+            "Podesi odnos izmedju keyword i semantic searcha za hybrid search",
+            0.0,
+            1.0,
+            0.5,
+            0.1,
+            help="Koeficijent koji određuje koliko će biti zastupljena pretraga po ključnim rečima, a koliko po semantičkom značenju. 0-0.4 pretezno Kljucne reci , 0.5 podjednako, 0.6-1 pretezno semanticko znacenje",
+        )
 
 
 def read_csv(upit):
@@ -330,3 +332,12 @@ def hybrid_query(upit):
         odgovor += member.page_content + "\n\n"
 
     return odgovor
+
+
+deployment_environment = os.environ.get("DEPLOYMENT_ENVIRONMENT")
+
+if deployment_environment == "Streamlit":
+    name, authentication_status, username = positive_login(main, " ")
+else:
+    if __name__ == "__main__":
+        main()
