2b695709dde939213a2251efe521a2c6a9b1ad5e
nemper <nemanja.perun98@gmail.com>
2023-10-21 14:22:07 +0700

    csv upload

--- write-in-style/csvtest.py
+++ write-in-style/csvtest.py
@@ -3,22 +3,30 @@ from langchain.agents import AgentType
 from langchain.agents import create_csv_agent
 from langchain.chat_models import ChatOpenAI
 from myfunc.mojafunkcija import init_cond_llm
+import io
 
 st.subheader("Testiranje modela na osnovu csv fajla")
 st.caption("Ver. 21.10.23")
 st.divider()
 model, temp = init_cond_llm()
-with st.form("my_form"):
-    upit = st.text_area("Sistem: ", value="Pisi iskljucivo na srpskom jeziku. ")
-    posalji = st.form_submit_button("Posalji")
-if posalji:
-    agent = create_csv_agent(
-        ChatOpenAI(temperature=temp, model=model),
-        "pravilnik.csv",
-        verbose=True,
-        agent_type=AgentType.OPENAI_FUNCTIONS,
-        handle_parsing_errors=True,
-    )
+uploaded_file = st.file_uploader(
+    "Choose a CSV file", accept_multiple_files=False, type="csv", key="csv_key"
+)
+if uploaded_file is not None:
+    with io.open(uploaded_file.name, "wb") as file:
+        file.write(uploaded_file.getbuffer())
+    with st.form("my_form"):
+        upit = st.text_area("Sistem: ", value="Pisi iskljucivo na srpskom jeziku. ")
+        posalji = st.form_submit_button("Posalji")
 
-    odgovor = agent.run(upit)
-    st.write(odgovor)
+        if posalji:
+            agent = create_csv_agent(
+                ChatOpenAI(temperature=temp, model=model),
+                "pravilnik.csv",
+                verbose=True,
+                agent_type=AgentType.OPENAI_FUNCTIONS,
+                handle_parsing_errors=True,
+            )
+
+            odgovor = agent.run(upit)
+            st.write(odgovor)
