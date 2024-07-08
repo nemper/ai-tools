98f1dfe6761afb6713942ce503d6418a23bb395a
nemper <nemanja.perun98@gmail.com>
2024-07-08 12:33:10 +0200

    all2csv

--- gdrive2pinecone/main.py
+++ gdrive2pinecone/main.py
@@ -3,6 +3,8 @@ import json
 import streamlit as st
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+import pandas as pd
+from io import StringIO
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
@@ -15,7 +17,7 @@ def get_drive_service():
 
 def list_drive_files():
     service = get_drive_service()
-    results = service.files().list(pageSize=10, fields="nextPageToken, files(id, name, webViewLink)").execute()
+    results = service.files().list(pageSize=100, fields="nextPageToken, files(id, name, webViewLink)").execute()
     items = results.get('files', [])
     return items
 
@@ -26,5 +28,23 @@ if st.button('List Google Drive Files'):
     if not files:
         st.write('No files found.')
     else:
-        for file in files:
-            st.write(f"{file['name']} - [Open]({file['webViewLink']})")
+        # Create a DataFrame
+        df = pd.DataFrame(files)
+        df = df[['name', 'webViewLink']]
+        df.columns = ['File Name', 'URL']
+
+        # Convert DataFrame to CSV with specified encoding
+        csv_buffer = StringIO()
+        df.to_csv(csv_buffer, index=False, encoding='utf-8-sig')
+        csv_data = csv_buffer.getvalue()
+
+        # Write "Done" message
+        st.write("Done")
+
+        # Provide a download button
+        st.download_button(
+            label="Download CSV",
+            data=csv_data,
+            file_name='google_drive_files.csv',
+            mime='text/csv'
+        )
